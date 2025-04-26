7a0233c723522a4189e5f9884197cd0f8094f0ac
rouaida3 <159000943+rouaida3@users.noreply.github.com>
2025-04-26 10:32:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,166 +1,195 @@
 $('button.encode, button.decode').click(function(event) {
-    event.preventDefault();
-  });
-  
-  function previewDecodeImage() {
-    var file = document.querySelector('input[name=decodeFile]').files[0];
-  
-    previewImage(file, ".decode canvas", function() {
+  event.preventDefault();
+});
+
+// === Image preview functions ===
+function previewDecodeImage() {
+  var file = document.querySelector('input[name=decodeFile]').files[0];
+  previewImage(file, ".decode canvas", function() {
       $(".decode").fadeIn();
-    });
-  }
-  
-  function previewEncodeImage() {
-    var file = document.querySelector("input[name=baseFile]").files[0];
-  
-    $(".images .nulled").hide();
-    $(".images .message").hide();
-  
-    previewImage(file, ".original canvas", function() {
+  });
+}
+
+function previewEncodeImage() {
+  var file = document.querySelector("input[name=baseFile]").files[0];
+  $(".images .nulled").hide();
+  $(".images .message").hide();
+  previewImage(file, ".original canvas", function() {
       $(".images .original").fadeIn();
       $(".images").fadeIn();
-    });
-  }
-  
-  function previewImage(file, canvasSelector, callback) {
-    var reader = new FileReader();
-    var image = new Image;
-    var $canvas = $(canvasSelector);
-    var context = $canvas[0].getContext('2d');
-  
-    if (file) {
+  });
+}
+
+function previewImage(file, canvasSelector, callback) {
+  var reader = new FileReader();
+  var image = new Image;
+  var $canvas = $(canvasSelector);
+  var context = $canvas[0].getContext('2d');
+
+  if (file) {
       reader.readAsDataURL(file);
-    }
-  
-    reader.onloadend = function () {
-      image.src = URL.createObjectURL(file);
-  
-      image.onload = function() {
-        $canvas.prop({
-          'width': image.width,
-          'height': image.height
-        });
-  
-        context.drawImage(image, 0, 0);
-  
-        callback();
-      }
-    }
   }
+
+  reader.onloadend = function () {
+      image.src = URL.createObjectURL(file);
+      image.onload = function() {
+          $canvas.prop({
+              'width': image.width,
+              'height': image.height
+          });
+          context.drawImage(image, 0, 0);
+          callback();
+      }
+  }
+}
+
+// === XOR encryption and decryption ===
+function xorEncrypt(message, key) {
+  var encrypted = "";
+  for (var i = 0; i < message.length; i++) {
+      var keyChar = key[i % key.length];
+      encrypted += String.fromCharCode(message.charCodeAt(i) ^ keyChar.charCodeAt(0));
+  }
+  return encrypted;
+}
+
+function xorDecrypt(encryptedMessage, key) {
+  return xorEncrypt(encryptedMessage, key); // XOR is symmetric
+}
+
+// === Obfuscation ===
+function obfuscateBinary(binaryMessage) {
+  var obfuscated = "";
+  for (var i = 0; i < binaryMessage.length; i += 8) {
+      obfuscated += binaryMessage.substr(i, 8);
+      obfuscated += Math.round(Math.random()) + "" + Math.round(Math.random());
+  }
+  return obfuscated;
+}
+
+function deobfuscateBinary(obfuscatedBinaryMessage) {
+  var cleanBinary = "";
+  for (var i = 0; i < obfuscatedBinaryMessage.length; i += 10) {
+      cleanBinary += obfuscatedBinaryMessage.substr(i, 8);
+  }
+  return cleanBinary;
+}
+
+// === ENCODING FUNCTION ===
+function encodeMessage() {
+  $(".error").hide();
+  $(".binary").hide();
   
-  function encodeMessage() {
-    $(".error").hide();
-    $(".binary").hide();
-  
-    var text = $("textarea.message").val();
-  
-    var $originalCanvas = $('.original canvas');
-    var $nulledCanvas = $('.nulled canvas');
-    var $messageCanvas = $('.message canvas');
-  
-    var originalContext = $originalCanvas[0].getContext("2d");
-    var nulledContext = $nulledCanvas[0].getContext("2d");
-    var messageContext = $messageCanvas[0].getContext("2d");
-  
-    var width = $originalCanvas[0].width;
-    var height = $originalCanvas[0].height;
-  
-    // Check if the image is big enough to hide the message
-    if ((text.length * 8) > (width * height * 3)) {
-      $(".error")
-        .text("Text too long for chosen image....")
-        .fadeIn();
-  
+  var text = $("textarea.message").val();
+  var key = $("input[name=key]").val();
+
+  var $originalCanvas = $('.original canvas');
+  var $nulledCanvas = $('.nulled canvas');
+  var $messageCanvas = $('.message canvas');
+
+  var originalContext = $originalCanvas[0].getContext("2d");
+  var nulledContext = $nulledCanvas[0].getContext("2d");
+  var messageContext = $messageCanvas[0].getContext("2d");
+
+  var width = $originalCanvas[0].width;
+  var height = $originalCanvas[0].height;
+
+  if ((text.length * 8) > (width * height * 3)) {
+      $(".error").text("Text too long for chosen image....").fadeIn();
       return;
-    }
-  
-    $nulledCanvas.prop({
-      'width': width,
-      'height': height
-    });
-  
-    $messageCanvas.prop({
-      'width': width,
-      'height': height
-    });
-  
-    // Normalize the original image and draw it
-    var original = originalContext.getImageData(0, 0, width, height);
-    var pixel = original.data;
-    for (var i = 0, n = pixel.length; i < n; i += 4) {
-      for (var offset =0; offset < 3; offset ++) {
-        if(pixel[i + offset] %2 != 0) {
-          pixel[i + offset]--;
-        }
+  }
+
+  $nulledCanvas.prop({ 'width': width, 'height': height });
+  $messageCanvas.prop({ 'width': width, 'height': height });
+
+  // Normalize the image
+  var original = originalContext.getImageData(0, 0, width, height);
+  var pixel = original.data;
+  for (var i = 0, n = pixel.length; i < n; i += 4) {
+      for (var offset = 0; offset < 3; offset++) {
+          if (pixel[i + offset] % 2 != 0) {
+              pixel[i + offset]--;
+          }
       }
-    }
-    nulledContext.putImageData(original, 0, 0);
-  
-    // Convert the message to a binary string
-    var binaryMessage = "";
-    for (i = 0; i < text.length; i++) {
-      var binaryChar = text[i].charCodeAt(0).toString(2);
-  
-      // Pad with 0 until the binaryChar has a lenght of 8 (1 Byte)
-      while(binaryChar.length < 8) {
-        binaryChar = "0" + binaryChar;
+  }
+  nulledContext.putImageData(original, 0, 0);
+
+  // Step 1: Encrypt text
+  var encryptedText = xorEncrypt(text, key);
+
+  // Step 2: Convert encrypted text to binary
+  var binaryMessage = "";
+  for (var i = 0; i < encryptedText.length; i++) {
+      var binaryChar = encryptedText[i].charCodeAt(0).toString(2);
+      while (binaryChar.length < 8) {
+          binaryChar = "0" + binaryChar;
       }
-  
       binaryMessage += binaryChar;
-    }
-    $('.binary textarea').text(binaryMessage);
-  
-    // Apply the binary string to the image and draw it
-    var message = nulledContext.getImageData(0, 0, width, height);
-    pixel = message.data;
-    counter = 0;
-    for (var i = 0, n = pixel.length; i < n; i += 4) {
-      for (var offset =0; offset < 3; offset ++) {
-        if (counter < binaryMessage.length) {
-          pixel[i + offset] += parseInt(binaryMessage[counter]);
-          counter++;
-        }
-        else {
-          break;
-        }
+  }
+  $('.binary textarea').text(binaryMessage); // Show clean binary
+
+  // Step 3: Obfuscate the binary
+  var obfuscatedBinaryMessage = obfuscateBinary(binaryMessage);
+  $('.binary_Obf_textarea').text(obfuscatedBinaryMessage); // Show obfuscated binary
+
+  // Step 4: Hide obfuscated binary into the image
+  var message = nulledContext.getImageData(0, 0, width, height);
+  pixel = message.data;
+  var counter = 0;
+  for (var i = 0, n = pixel.length; i < n; i += 4) {
+      for (var offset = 0; offset < 3; offset++) {
+          if (counter < obfuscatedBinaryMessage.length) {
+              pixel[i + offset] += parseInt(obfuscatedBinaryMessage[counter]);
+              counter++;
+          }
       }
-    }
-    messageContext.putImageData(message, 0, 0);
-  
-    $(".binary").fadeIn();
-    $(".images .nulled").fadeIn();
-    $(".images .message").fadeIn();
-  };
-  
-  function decodeMessage() {
-    var $originalCanvas = $('.decode canvas');
-    var originalContext = $originalCanvas[0].getContext("2d");
-  
-    var original = originalContext.getImageData(0, 0, $originalCanvas.width(), $originalCanvas.height());
-    var binaryMessage = "";
-    var pixel = original.data;
-    for (var i = 0, n = pixel.length; i < n; i += 4) {
-      for (var offset =0; offset < 3; offset ++) {
-        var value = 0;
-        if(pixel[i + offset] %2 != 0) {
-          value = 1;
-        }
-  
-        binaryMessage += value;
+  }
+  messageContext.putImageData(message, 0, 0);
+
+  $(".binary").fadeIn(); // Show binary section
+  $(".images .nulled").fadeIn(); // Show nulled image
+  $(".images .message").fadeIn(); // Show message image
+}
+
+// === DECODING FUNCTION ===
+function decodeMessage() {
+  var $originalCanvas = $('.decode canvas');
+  var originalContext = $originalCanvas[0].getContext("2d");
+
+  var original = originalContext.getImageData(0, 0, $originalCanvas.width(), $originalCanvas.height());
+  var pixel = original.data;
+
+  // Step 1: Extract bits
+  var binaryMessage = "";
+  for (var i = 0, n = pixel.length; i < n; i += 4) {
+      for (var offset = 0; offset < 3; offset++) {
+          var value = 0;
+          if (pixel[i + offset] % 2 != 0) {
+              value = 1;
+          }
+          binaryMessage += value;
       }
-    }
-  
-    var output = "";
-    for (var i = 0; i < binaryMessage.length; i += 8) {
+  }
+
+  // Step 2: Deobfuscate
+  var cleanBinaryMessage = deobfuscateBinary(binaryMessage);
+
+  // Step 3: Convert binary to text
+  var output = "";
+  for (var i = 0; i < cleanBinaryMessage.length; i += 8) {
       var c = 0;
       for (var j = 0; j < 8; j++) {
-        c <<= 1;
-        c |= parseInt(binaryMessage[i + j]);
+          c <<= 1;
+          c |= parseInt(cleanBinaryMessage[i + j]);
       }
-  
       output += String.fromCharCode(c);
-    }
-  
-    $('.binary-decode textarea').text(output);
-    $('.binary-decode').fadeIn();
-  };
+  }
+
+  // Step 4: Decrypt using key
+  var key = $("input[name=key]").val();
+  var decryptedMessage = xorDecrypt(output, key);
+
+  // Step 5: Display result
+  $('.binary-decode textarea').text(decryptedMessage);
+  $('.binary-decode').fadeIn();
+}
